f783930c0d1d03ccc6a932fae92a25600f84e8a2
tom <tomallpress@hotmail.com>
2018-02-05 15:07:16 +0000

    test passing for Card class constructor, for rank and suit

--- Test/CardTest.java
+++ Test/CardTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 
 public class CardTest {
     @Test
-    public void newCard() {
+    public void newCardSuit() {
 
         Card card = new Card(Card.Rank.ACE, Card.Suit.SPADE);
 
         Assert.assertEquals(Card.Suit.SPADE, card.suit);
     }
+    @Test
+    public void newCardRank() {
+
+        Card card = new Card(Card.Rank.TWO, Card.Suit.HEART);
+
+        Assert.assertEquals(Card.Rank.TWO, card.rank);
+    }
 }
+
