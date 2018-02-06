237e74570031e5a189e0730f216b73eee66a928e
joepike <joseph.l.pike@gmail.com>
2018-02-06 17:48:03 +0000

    passing test for is there a pair in hand. failing test for return highest pair

--- Test/EvaluatorTest.java
+++ Test/EvaluatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class EvaluatorTest {
@@ -32,22 +33,21 @@ public class EvaluatorTest {
 
     }
 
-//    @Test public void pairOnePlayer() {
-//
-//        Card card1 = new Card(Rank.JACK, Suit.HEART);
-//        Card card2 = new Card(Rank.JACK, Suit.HEART);
-//
-////        System.out.println(card1.rank);
-//
-//        ArrayList<Card> cards = new ArrayList<Card>();
-//        cards.add(card1);
-//        cards.add(card2);
-//
-//        Evaluator evaluator = new Evaluator();
-//
-//        evaluator.hand = cards;
-//
-//        Assert.assertEquals(true, evaluator.pair());
-//
-//    }
+    @Test public void pairOnePlayer() {
+
+        Card card3 = new Card(Rank.SIX, Suit.SPADE);
+        Card card4 = new Card(Rank.SIX, Suit.HEART);
+//        System.out.println(card1.rank);
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+        cards.add(card3);
+        cards.add(card4);
+
+        Evaluator evaluator = new Evaluator();
+
+        evaluator.hand = cards;
+
+        Assert.assertEquals(true, evaluator.pair());
+
+    }
 }
