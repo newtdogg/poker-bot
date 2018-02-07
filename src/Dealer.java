f44271c61636c6a365a03be2143fc4e329ab8245
tom <tomallpress@hotmail.com>
2018-02-07 14:56:13 +0000

    refactored dealing the flop

--- src/Dealer.java
+++ src/Dealer.java
@@ -26,21 +26,13 @@ public class Dealer {
     }
 
     public void dealFlop(Bot bot) {
-        int index1 = r.nextInt(this.deck.size() - 3);
-        int index2 = r.nextInt(this.deck.size() - 4);
-        int index3 = r.nextInt(this.deck.size() - 5);
-        Card card1 = this.deck.get(index1);
-        this.deck.remove(card1);
-        this.board.add(card1);
-        bot.playableHand.add(card1);
-        Card card2 = this.deck.get(index2);
-        this.deck.remove(card2);
-        this.board.add(card2);
-        bot.playableHand.add(card2);
-        Card card3 = this.deck.get(index3);
-        this.deck.remove(card3);
-        this.board.add(card3);
-        bot.playableHand.add(card3);
+        for (int i = 3; i <= 5; i++) {
+            int index = r.nextInt(this.deck.size() - i);
+            Card card = this.deck.get(index);
+            this.deck.remove(card);
+            this.board.add(card);
+            bot.playableHand.add(card);
+        }
     }
 
     public void dealTurn(Bot bot){
