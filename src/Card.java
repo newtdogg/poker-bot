7bf4ccd326876e535ffe00495d3626acb02e96db
kkumshayev <kumshkesh@gmail.com>
2018-02-07 12:34:00 +0000

    sorting an arraylist of cards by rank tested and working

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Card {
 
     public Rank rank;
@@ -7,4 +9,14 @@ public class Card {
         this.rank = inputRank;
         this.suit = inputSuit;
     }
+
+    public static Comparator<Card> CardRankComparator = new Comparator<Card>() {
+        @Override
+        public int compare(Card c1, Card c2) {
+            int rank1 = Rank.valueOf(c1.rank.name()).ordinal();
+            int rank2 = Rank.valueOf(c2.rank.name()).ordinal();
+
+            return rank2-rank1;
+        }
+    };
 }
